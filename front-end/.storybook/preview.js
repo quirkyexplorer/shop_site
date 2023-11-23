10d78af2934e703a24f6bf901cd2f006ba5fae37
quirkexplorer <quirkyexplorer@gmail.com>
2023-11-22 22:32:08 -0700

    story book working without themeProvider

--- front-end/.storybook/preview.js
+++ front-end/.storybook/preview.js
@@ -1,3 +1,15 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+/* TODO: update import for your custom theme configurations */
+// import { lightTheme, darkTheme } from '../path/to/themes';
+
+/* TODO: replace with your own global styles, or remove */
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  `;
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -9,6 +21,17 @@ const preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+    // themes: {
+    //   light: lightTheme,
+    //   dark: darkTheme,
+    // },
+    // defaultTheme: 'light',
+    // Provider: ThemeProvider,
+    GlobalStyles,
+    })]
 };
 
 export default preview;
