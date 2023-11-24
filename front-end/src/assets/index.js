6a7217ec5ba9411b603f934b0281a02e97bb74c7
quirkexplorer <quirkyexplorer@gmail.com>
2023-11-24 01:08:09 -0700

    'added hero images and css rules, need responsiveness'

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -3,7 +3,11 @@ import ex2 from './ex2.jpg'
 import ex3 from './ex3.jpg'
 import ex4 from './ex4.jpg'
 import ex5 from './ex5.jpg'
+import hero1 from './hero1.jpg'
+import hero2 from './hero2.jpg'
 
 const images = [ex, ex2, ex3, ex4, ex5];
 
-export default images;
+const heroes = [hero1, hero2];
+
+export { images, heroes };
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -2,17 +2,14 @@ import React from 'react';
 import Carrousel from '../carousel/Carousel';
 import Header from '../header/Header';
 import Body from '../body/Body';
-import EditPage from './EditPage';
+import HeroSection from './HeroSection';
 
 export default function Home() {
   return (
     <div>
-      <Header></Header>
-      <h2 style={{textAlign: 'center'}}>
-        Welcome to home page
-      </h2>
-      <Body></Body>
-      <Carrousel></Carrousel>
+      <Header/>
+      <HeroSection/>
+      <Carrousel/>
     </div>
   )
 }
